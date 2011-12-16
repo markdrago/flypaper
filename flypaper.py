54d3b5bc5af45e402e4362d613bc8d41604f86b7
Mark Drago <markdrago@gmail.com>
2011-12-16 18:54:31 -0500

    remove getters/setters, consolidate mercurial specific code

--- flypaper.py
+++ flypaper.py
@@ -7,15 +7,15 @@ import os
 
 class FlyPaper(object):
     def __init__(self, bugid_file, repodir, startdate):
-        self.bugid_file = bugid_file
-        self.repodir = repodir
-        self.startdate = startdate
+        self._bugid_file = bugid_file
+        self._repodir = repodir
+        self._startdate = startdate
 
     def show_bug_catchers(self):
-        self.buglist = BugList(self.bugid_file)
-        self.repo = Repo(self.repodir, self.startdate)
-        changesetlist = self.repo.get_full_changesetlist()
-        changesetlist.remove_changesets_without_matching_description(self.buglist.get_bugids())
+        self._buglist = BugList(self._bugid_file)
+        self._repo = RepoFactory.get_repo(self._repodir, self._startdate)
+        changesetlist = self._repo.get_full_changesetlist()
+        changesetlist.remove_changesets_without_matching_description(self._buglist.bugids)
         
         files = {}
         for changeset in changesetlist.changesets:
@@ -32,7 +32,7 @@ class ChangesetList(object):
     def __init__(self):
         self.changesets = []
     
-    def add(self,  changeset):
+    def add(self, changeset):
         self.changesets.append(changeset)
 
     def remove_changesets_without_matching_description(self, bugids):
@@ -48,59 +48,57 @@ class ChangesetList(object):
         self.changesets = bug_changesets
 
 class Changeset(object):
-    def __init__(self, repo, hexid, description=''):
-        self.repo = repo
-        self.hexid = hexid
-        self.set_description(description)
-
-    def get_hexid(self):
-        return self.hexid
-
-    def set_description(self, description):
+    def __init__(self, repo, commitid, description=''):
+        self._repo = repo
+        self.commitid = commitid
         self.description = description
-    
-    def get_description(self):
-        return self.description
 
     def description_contains(self, needle):
         return needle in self.description
 
     def get_modified_files(self):
-        return self.repo.get_files_modified_in_changeset(self.hexid)
+        return self._repo.get_files_modified_in_changeset(self.commitid)
 
     def get_score_for_fix_on_date(self, date):
         return 1
 
-class Repo(object):
+class RepoFactory(object):
+    @classmethod
+    def get_repo(clazz, repodir, startdate):
+        if os.path.isdir(repodir + "/.hg"):
+            return MercurialRepo(repodir, startdate)
+        else:
+            raise Exception("No support repository found in: %s" % (repodir,))
+
+class MercurialRepo(object):
     def __init__(self, repodir, startdate):
-        self.repodir = repodir
-        self.startdate = startdate
-        self.get_full_changesetlist()
+        self._repodir = repodir
+        self._startdate = startdate
 
     def get_full_changesetlist(self):
         "return ChangesetList of all changesets since startdate"
-        cmd = 'hg log -d ">' + self.startdate + '" --template "{node}|{desc|firstline}\n"'
+        cmd = 'hg log -d ">' + self._startdate + '" --template "{node}|{desc|firstline}\n"'
         result = self.get_command_output(cmd)
 
         changeset_list = ChangesetList()
         for line in result.split('\n'):
             if line.strip() == '':
                 continue
-            (hexid, desc) = line.split('|', 1)
-            changeset_list.add(Changeset(self, hexid, desc))
+            (commitid, desc) = line.split('|', 1)
+            changeset_list.add(Changeset(self, commitid, desc))
         return changeset_list
 
-    def get_files_modified_in_changeset(self, hexid):
+    def get_files_modified_in_changeset(self, commitid):
         "return list of filenames modified in changeset"
         #note: getting files split on space may be a problem
-        cmd = "hg log -r %s --template \"{files}\"" % (hexid,)
+        cmd = "hg log -r %s --template \"{files}\"" % (commitid,)
         result = self.get_command_output(cmd)
         return result.split(' ')
 
     def get_command_output(self, cmd):
         "run a shell command and get the output"
         oldpath = os.getcwd()
-        os.chdir(self.repodir)
+        os.chdir(self._repodir)
 
         proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         result = proc.communicate()[0]
@@ -111,28 +109,25 @@ class Repo(object):
 class BugList(object):
     def __init__(self, bugid_file):
         self.bugids = []
-        self.bugid_file = bugid_file
+        self._bugid_file = bugid_file
         self.read_bug_list()
 
     def read_bug_list(self):
-        for line in self.bugid_file:
+        for line in self._bugid_file:
             self.bugids.append(line.strip())
 
-    def get_bugids(self):
-        return self.bugids
-
 if __name__ == '__main__':
-    description = 'Flypaper shows you the files which attract bugs'
+    description = 'Flypaper shows you the files which tend to attract bugs'
     parser = argparse.ArgumentParser(description)
     parser.add_argument('--buglist', default=sys.stdin,
                         type=argparse.FileType('r'),
                         help='a file which contains bug identifiers')
-    parser.add_argument('--hgrepo', default=None, type=unicode,
-                        help='directory which contains a mercurial repo')
+    parser.add_argument('--repo', default=None, type=unicode,
+                        help='directory which contains a code repository')
     parser.add_argument('--startdate', default='2011-01-01', type=unicode,
                         help='date to start looking in the repository')
     args = parser.parse_args()
 
-    flypaper = FlyPaper(args.buglist, args.hgrepo, args.startdate)
+    flypaper = FlyPaper(args.buglist, args.repo, args.startdate)
     flypaper.show_bug_catchers()
 
