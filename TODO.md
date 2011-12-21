1982cb4c5723ed83a0d56436dc3c50ba7a1e3d94
Mark Drago <markdrago@gmail.com>
2011-12-20 19:19:48 -0500

    a lot of changes, support bugginess formula w/ recency, do not overcount for >1 commit/bug

---
+++ TODO.md
@@ -0,0 +1,10 @@
+TODO
+====
+
+* Support git
+* Limit output to N most buggy files
+* Control bugginess calculation based on severity of bugs
+* Have some way of showing which bugs are included in the calculation
+* Some way of controlling the impact of recency on the calculation
+* Default startdate to 1 year ago or something reasonable
+
--- flypaper.py
+++ flypaper.py
@@ -2,8 +2,11 @@
 
 import subprocess
 import argparse
+import math
 import sys
 import os
+
+from datetime import datetime
 
 class FlyPaper(object):
     def __init__(self, bugid_file, repodir, startdate):
@@ -14,53 +17,147 @@ class FlyPaper(object):
     def show_bug_catchers(self):
         self._buglist = BugList(self._bugid_file)
         self._repo = RepoFactory.get_repo(self._repodir, self._startdate)
-        changesetlist = self._repo.get_full_changesetlist()
-        changesetlist.remove_changesets_without_matching_description(self._buglist.bugids)
+        self._changesetlist = self._repo.get_full_changesetlist()
 
-        files = {}
-        for changeset in changesetlist.changesets:
-            for filename in changeset.get_modified_files():
-                if filename not in files:
-                    files[filename] = 1
-                else:
-                    files[filename] += 1
+        self._match_bugs_with_changesets()
+        self._changesetlist.remove_changesets_which_do_not_fix_a_bug()
 
-        for filename in files:
-            print str(files[filename]) + "  " + filename
+        self._build_buggy_file_list()
+        results = self._get_buggy_files_sorted_by_bugginess()
+        self._print_results(results)
 
-class ChangesetList(object):
-    def __init__(self):
-        self.changesets = []
+    def _build_buggy_file_list(self):
+        self._buggy_file_list = BuggyFileList()
+        for changeset in self._changesetlist.changesets.values():
+            for filename in changeset.modified_files:
+                for bug in changeset.bugs_fixed:
+                    self._buggy_file_list.add_buggy_file(bug, filename)
 
-    def add(self, changeset):
-        self.changesets.append(changeset)
-
-    def remove_changesets_without_matching_description(self, bugids):
-        bug_changesets = []
-        for changeset in self.changesets:
-            found_bug = False
-            for bugid in bugids:
+    def _match_bugs_with_changesets(self):
+        for changeset in self._changesetlist.changesets.values():
+            for bugid in self._buglist.bugs.keys():
                 if changeset.description_contains(bugid):
-                    found_bug = True
-                    break
-            if found_bug:
-                bug_changesets.append(changeset)
-        self.changesets = bug_changesets
+                    bug = self._buglist.bugs[bugid]
+                    bug.add_fixing_changeset(changeset)
+                    changeset.add_bug_fixed(bug)
+
+    def _get_buggy_files_sorted_by_bugginess(self):
+        scores = {}
+        for buggyfile in self._buggy_file_list.filenames.values():
+            score = buggyfile.get_score(self._startdate)
+            if score not in scores:
+                scores[score] = []
+            scores[score].append(buggyfile)
+        return scores
+
+    def _print_results(self, results):
+        scores = results.keys()
+        scores.sort()
+        for score in scores:
+            buggyfiles = results[score]
+            buggyfiles.sort(cmp=lambda x, y: cmp(x.filename, y.filename))
+            for buggyfile in buggyfiles:
+                print "%.03f" % score + " " + buggyfile.filename
+
+class Bug(object):
+    def __init__(self, bugid):
+        self.score = None
+        self.bugid = bugid
+        self.fixing_changesets = set()
+
+    def add_fixing_changeset(self, changeset):
+        self.fixing_changesets.add(changeset)
+
+    def get_score(self, startdate):
+        if self.score is None:
+            self.score = max([chg.get_score(startdate) for chg in self.fixing_changesets])
+        return self.score
+    
+    def __str__(self):
+        return str(self.bugid)
+
+class BugList(object):
+    def __init__(self, bugid_file):
+        self._bugid_file = bugid_file
+        self.read_bug_list()
+
+    def read_bug_list(self):
+        self.bugs = {}
+        for line in self._bugid_file:
+            bugid = line.strip()
+            bug = Bug(bugid)
+            self.bugs[bugid] = bug
+
+class BuggyFile(object):
+    def __init__(self, filename):
+        self.score = None
+        self.filename = filename
+        self.bugs = {}
+
+    def add_bug(self, bug):
+        if bug.bugid not in self.bugs:
+            self.bugs[bug.bugid] = bug
+
+    def get_score(self, startdate):
+        if self.score is None:
+            self.score = sum([bug.get_score(startdate) for bug in self.bugs.values()])
+        return self.score
+
+class BuggyFileList(object):
+    def __init__(self):
+        self.filenames = {}
+
+    def add_buggy_file(self, bug, filename):
+        if filename not in self.filenames:
+            self.filenames[filename] = BuggyFile(filename)
+        self.filenames[filename].add_bug(bug)
 
 class Changeset(object):
-    def __init__(self, repo, commitid, description=''):
+    def __init__(self, repo, commitid, date, description=''):
+        self.score = None
         self._repo = repo
         self.commitid = commitid
         self.description = description
+        self.date = date
+        self.bugs_fixed = set()
+        self.modified_files = set()
 
     def description_contains(self, needle):
         return needle in self.description
 
-    def get_modified_files(self):
-        return self._repo.get_files_modified_in_changeset(self.commitid)
+    def add_bug_fixed(self, bug):
+        self.bugs_fixed.add(bug)
 
-    def get_score_for_fix_on_date(self, date):
-        return 1
+    def add_modified_file(self, filename):
+        self.modified_files.add(filename)
+
+    def get_score(self, startdate):
+        if self.score is None:
+            self.score = self._calculate_score(startdate)
+        return self.score
+
+    def _calculate_score(self, startdate):
+        today = datetime.today().replace(hour=0, minute=0, second=0)
+        total_diff = today - startdate
+        changeset_diff = self.date - startdate
+
+        time_ratio = changeset_diff.total_seconds() / total_diff.total_seconds()
+
+        return 1 / (1 + math.exp((-3 * time_ratio) + 3))
+
+class ChangesetList(object):
+    def __init__(self):
+        self.changesets = {}
+
+    def add(self, changeset):
+        self.changesets[changeset.commitid] = changeset
+
+    def remove_changesets_which_do_not_fix_a_bug(self):
+        changesets_with_bugs_fixed = {}
+        for commit_id in self.changesets:
+            if len(self.changesets[commit_id].bugs_fixed) > 0:
+                changesets_with_bugs_fixed[commit_id] = self.changesets[commit_id]
+        self.changesets = changesets_with_bugs_fixed
 
 class RepoFactory(object):
     @classmethod
@@ -77,23 +174,37 @@ class MercurialRepo(object):
 
     def get_full_changesetlist(self):
         "return ChangesetList of all changesets since startdate"
-        cmd = 'hg log -d ">' + self._startdate + '" --template "{node}|{desc|firstline}\n"'
+        changeset_list = ChangesetList()
+        self.add_changesets_with_dates_and_descriptions(changeset_list)
+        self.add_modified_files_to_changeset_list(changeset_list)
+        return changeset_list
+
+    def add_modified_files_to_changeset_list(self, changeset_list):
+        startdatestr = self._startdate.strftime('%Y-%m-%d')
+        #note: getting files split on space may be a problem
+        cmd = 'hg log -d ">' + startdatestr + '" --template "{node}|{files}\n"'
         result = self.get_command_output(cmd)
 
-        changeset_list = ChangesetList()
         for line in result.split('\n'):
             if line.strip() == '':
                 continue
-            (commitid, desc) = line.split('|', 1)
-            changeset_list.add(Changeset(self, commitid, desc))
-        return changeset_list
+            (commitid, filenames) = line.split('|', 1)
+            for filename in filenames.split(' '):
+                filename = filename.strip()
+                if len(filename) > 0:
+                    changeset_list.changesets[commitid].add_modified_file(filename)
 
-    def get_files_modified_in_changeset(self, commitid):
-        "return list of filenames modified in changeset"
-        #note: getting files split on space may be a problem
-        cmd = "hg log -r %s --template \"{file_mods} {file_adds} {file_dels}\"" % (commitid,)
+    def add_changesets_with_dates_and_descriptions(self, changeset_list):
+        startdatestr = self._startdate.strftime('%Y-%m-%d')
+        cmd = 'hg log -d ">' + startdatestr + '" --template "{node}|{date|shortdate}|{desc|firstline}\n"'
         result = self.get_command_output(cmd)
-        return [f for f in result.split(' ') if len(f.strip()) > 0]
+
+        for line in result.split('\n'):
+            if line.strip() == '':
+                continue
+            (commitid, datestr, desc) = line.split('|', 2)
+            date = datetime.strptime(datestr, '%Y-%m-%d')
+            changeset_list.add(Changeset(self, commitid, date, desc))
 
     def get_command_output(self, cmd):
         "run a shell command and get the output"
@@ -105,16 +216,6 @@ class MercurialRepo(object):
 
         os.chdir(oldpath)
         return result
-
-class BugList(object):
-    def __init__(self, bugid_file):
-        self.bugids = []
-        self._bugid_file = bugid_file
-        self.read_bug_list()
-
-    def read_bug_list(self):
-        for line in self._bugid_file:
-            self.bugids.append(line.strip())
 
 if __name__ == '__main__':
     description = 'Flypaper shows you the files which tend to attract bugs'
@@ -128,6 +229,8 @@ if __name__ == '__main__':
                         help='date to start looking in the repository')
     args = parser.parse_args()
 
-    flypaper = FlyPaper(args.buglist, args.repo, args.startdate)
+    flypaper = FlyPaper(args.buglist,
+                        args.repo,
+                        datetime.strptime(args.startdate, '%Y-%m-%d'))
     flypaper.show_bug_catchers()
 
