220c24aa91313ce5546c8d86e6469e2c603b148e
Mark Drago <markdrago@gmail.com>
2011-12-22 18:31:40 -0500

    add a few comments

--- flypaper/bug.py
+++ flypaper/bug.py
@@ -8,6 +8,9 @@ class Bug(object):
         self.fixing_changesets.add(changeset)
 
     def get_score(self, startdate):
+        '''the score for a bug is the score for the latest changeset which
+           fixes that bug, since the score for a changeset is based on recency
+           alone, we just take the score for the highest scoring changeset'''
         if self.score is None:
             self.score = max([chg.get_score(startdate) for chg in self.fixing_changesets])
         return self.score
--- flypaper/buggyfile.py
+++ flypaper/buggyfile.py
@@ -9,6 +9,8 @@ class BuggyFile(object):
             self.bugs[bug.bugid] = bug
 
     def get_score(self, startdate):
+        '''the score for a file is the sum of the score for the bugs
+           which were in it'''
         if self.score is None:
             self.score = sum([bug.get_score(startdate) for bug in self.bugs.values()])
         return self.score
