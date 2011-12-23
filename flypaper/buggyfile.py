b836120812fd5030fe2dcf578cf98b55cfdca889
Mark Drago <markdrago@gmail.com>
2011-12-23 08:13:14 -0500

    fix bug with --showbugs

--- flypaper/buggyfile.py
+++ flypaper/buggyfile.py
@@ -1,18 +1,18 @@
 class BuggyFile(object):
     def __init__(self, filename):
         self._score = None
-        self._bugs = {}
+        self.bugs = {}
         self.filename = filename
 
     def add_bug(self, bug):
-        if bug.bugid not in self._bugs:
-            self._bugs[bug.bugid] = bug
+        if bug.bugid not in self.bugs:
+            self.bugs[bug.bugid] = bug
 
     def get_score(self, startdate):
         '''the score for a file is the sum of the score for the bugs
            which were in it'''
         if self._score is None:
-            self._score = sum([bug.get_score(startdate) for bug in self._bugs.values()])
+            self._score = sum([bug.get_score(startdate) for bug in self.bugs.values()])
         return self._score
 
 class BuggyFileFactory(object):
