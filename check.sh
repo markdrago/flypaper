dad034f57c8d0315eb391e34d3cc1e731bbfeda6
Mark Drago <markdrago@gmail.com>
2011-12-30 17:48:01 -0500

    add script to run unit tests and check pep8
    this can be used as a git hook to run at pre-commit time

---
+++ check.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#run unit tests
+cd flypaper
+python -m unittest discover
+test_result=$?
+cd ..
+
+#check pep8 style guidelines
+which pep8 > /dev/null
+if [ $? != 0 ]; then
+    echo 'install pep8: pip install pep8'
+else
+    find -name '*.py' | xargs pep8
+    pep8_result=$?
+fi
+
+if [ $test_result != 0 -o $pep8_result != 0 ]; then
+    exit 1
+fi
+
+exit 0
