11b982e0d99d242c8e8f833b27f570c40b2cdc6c
Mark Drago <markdrago@gmail.com>
2012-01-05 08:46:22 -0500

    add test around building list of buggy files

--- src/test/test_flypaper.py
+++ src/test/test_flypaper.py
@@ -11,23 +11,30 @@ class TestFlyPaper(unittest.TestCase):
     def setUp(self):
         self.fp = FlyPaper(None, None, datetime(2011, 1, 1, 0, 0, 0), False)
 
-    def test_matching_bugs_with_changesets(self):
-        #create two bugs
+        #create a few bugs and changesets
         bug1id = 'bug1'
-        bug1 = Bug(bug1id)
-        self.fp._buglist.add(bug1)
+        self.bug1 = Bug(bug1id)
+        self.fp._buglist.add(self.bug1)
 
         bug2id = 'bug2'
-        bug2 = Bug(bug2id)
-        self.fp._buglist.add(bug2)
+        self.bug2 = Bug(bug2id)
+        self.fp._buglist.add(self.bug2)
 
         #only one bug is part of a changeset
-        chg1 = Changeset('abc', datetime(2012, 1, 5, 8, 22, 0), bug1id)
-        self.fp._changesets.add(chg1)
+        self.chg1 = Changeset('abc', datetime(2012, 1, 5, 8, 22, 0), bug1id)
+        self.fp._changesets.add(self.chg1)
 
+    def test_matching_bugs_with_changesets(self):
         self.fp._match_bugs_with_changesets()
 
         #verify that only that one bug is linked with a changeset
-        self.assertEquals(set([chg1]), bug1.fixing_changesets)
-        self.assertEquals(0, len(bug2.fixing_changesets))
-        self.assertEquals(set([bug1]), chg1.bugs_fixed)
+        self.assertEquals(set([self.chg1]), self.bug1.fixing_changesets)
+        self.assertEquals(0, len(self.bug2.fixing_changesets))
+        self.assertEquals(set([self.bug1]), self.chg1.bugs_fixed)
+
+    def test_building_buggy_file_list(self):
+        self.chg1.add_modified_file('file1')
+        self.chg1.bugs_fixed.add(self.bug1)
+        self.fp._build_buggy_file_list()
+        self.assertEquals(1, len(self.fp._buggy_file_list._filenames))
+        self.assertIn('file1', self.fp._buggy_file_list._filenames)
