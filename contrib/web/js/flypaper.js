71044659d6a94ba53e0d700817a2819f0101a87e
Mark Drago <markdrago@gmail.com>
2012-01-10 22:13:22 -0500

    shorten the filenames displayed in the table

--- contrib/web/js/flypaper.js
+++ contrib/web/js/flypaper.js
@@ -26,8 +26,8 @@ function update_ui(results) {
         var file = results.files[i];
         rows += "<tr>";
         rows += "<td>" + file.score + "</td>";
-        rows += "<td>" + file.filename + "</td>";
-        rows += "<td>" + file.bugs + "</td>";
+        rows += "<td title=\"" + file.filename + "\">" + shorten_filename(file.filename) + "</td>";
+        rows += "<td>" + file.bugs.join(', ') + "</td>";
         rows += "</tr>";
     }
 
@@ -38,4 +38,27 @@ function update_ui(results) {
 
     //tell tablesorter that we changed the data underneath it
     table.trigger("update"); 
+}
+
+function shorten_filename(filename) {
+    //if the full filename isn't that long, just use it
+    if (filename.length < 75) {
+        return filename;
+    }
+
+    //split the filename up on slashes
+    var parts = filename.split('/');
+
+    //try and shorten the name and still include some path info
+    var shortname = parts[0];
+    if (parts.length > 0) {
+        shortname += "/.../" + parts[parts.length - 1];
+    }
+
+    //if the name is still too long, just use the basename
+    if (shortname.length > 75 && parts.length > 0) {
+        shortname = parts[parts.length - 1];
+    }
+
+    return shortname;
 }
